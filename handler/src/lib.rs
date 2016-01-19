3d3af25c68fcb0df14ea221e543e571f66ef1f11
YANG Xudong <xudong.yang@hde.co.jp>
2016-01-19 15:32:58 +0900

    Add while let demo.

--- handler/src/lib.rs
+++ handler/src/lib.rs
@@ -18,5 +18,33 @@ fn real_handle(event: String, context: String) -> i32 {
     println!("Event: {}", event);
     println!("Context: {}", context);
 
+    while_let_demo();
+
     return 0;
+}
+
+
+fn while_let_demo() {
+    println!("Start while_let demo.");
+
+    // Make `optional` of type `Option<i32>`
+    let mut optional = Some(0);
+
+    // This reads: "while `let` destructures `optional` into
+    // `Some(i)`, evaluate the block (`{}`). Else `break`.
+    while let Some(i) = optional {
+        if i > 9 {
+            println!("Greater than 9, quit!");
+            optional = None;
+        } else {
+            println!("`i` is `{:?}`. Try again.", i);
+            optional = Some(i + 1);
+        }
+        // ^ Less rightward drift and doesn't require
+        // explicitly handling the failing case.
+    }
+    // ^ `if let` had additional optional `else`/`else if`
+    // clauses. `while let` does not have these.
+
+    println!("while_let demo finished!");
 }
